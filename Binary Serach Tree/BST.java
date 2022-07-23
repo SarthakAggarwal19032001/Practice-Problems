d6b1d77ae753fa5db0bdf837820435e01839ce25
Sarthak Aggarwal <84911014+SarthakAggarwal19032001@users.noreply.github.com>
2022-07-23 12:42:16 +0530

    Add files via upload

--- Binary Serach Tree/BST.java
+++ Binary Serach Tree/BST.java
@@ -9,6 +9,7 @@ class BSTNode{
         right=null;
     }
 }
+
     class Operation{
         BSTNode root;
 
@@ -24,8 +25,7 @@ class BSTNode{
             else if(data>currnode.data){
                 currnode.right=insert(data,currnode.right);
             }
-            return currnode;
-            
+            return currnode;           
         }
 
         BSTNode insert(int data){
@@ -78,10 +78,83 @@ class BSTNode{
             return false;
         }
 
+
+        BSTNode max(BSTNode node){
+            BSTNode n=node;
+            while(node.right!=null){
+                 n=node;
+                node=node.right;
+            }
+            return n;
+        }
+
+        void delete(BSTNode currnode,BSTNode parent,boolean isleft,int val){
+            if(val<currnode.data){
+                delete(currnode.left, currnode, true, val);
+            }
+            else if(val>currnode.data){
+                delete(currnode.right, currnode, false, val);
+            }
+            // val found
+            else{
+            //case 1: it is leaf node simply delete it
+            if(currnode.left==null && currnode.right==null){
+                if(isleft){
+                    parent.left=null;
+                }
+                else{
+                    parent.right=null;
+                }
+            }
+            //case 2:  if left is null but currnode right is not null
+            else if(currnode.left==null && currnode.right!=null){
+                if(isleft){
+                    parent.left=currnode.right;
+                }
+                else{
+                    parent.right=currnode.right;
+                }
+            }
+
+            //case3: if right is null and left is not null of currnode
+            else if(currnode.left!=null && currnode.right==null){
+                if(isleft){
+                    parent.left=currnode.left;
+                }
+                else{parent.right=currnode.left;
+
+                }
+            }
+            //case 4: if left and right both are null...................
+            //We have two choices either find left max or right min i.e make it a leaf node and simply delete it
+            //Either now swap it and again make the recursive so that now other cases become valid and the node is deleted --or--- just copy its value of min or max in the currnode and call  the function now to delete that max or min element in rest of the node
+
+            else{
+                BSTNode leftmax=max(currnode.left);
+                int t=currnode.data;
+                currnode.data=leftmax.data;
+                leftmax.data=t;
+                delete(currnode.left, currnode, true, val);
+            }
+
+            /*
+                --or--
+                if (currentNode.left != null && currentNode.right != null) {
+                // 2 Choice a) Left Max b) Right Min
+                BSTNode temp = currentNode;
+                int leftMax = this.max(temp.left); // Leftmax
+                currentNode.data = leftMax;
+                delete(temp.left, currentNode, true, leftMax);
+            }
+
+
+
+
+
+            */
+        }
+        }
     }
-
-
-
 
 public class BST{
     public static void main(String[] args) {
@@ -105,5 +178,8 @@ public class BST{
         System.out.println("Through Iterative Searching.......");
         System.out.println(op.iterativesearch(bst, 9)?"Found":"Not Found");
         System.out.println(op.iterativesearch(bst, 100)?"Found":"Not Found");
+        System.out.println(" After Deletion");
+        op.delete(bst, bst, true, 8);
+        op.print(bst);
     }
 }
